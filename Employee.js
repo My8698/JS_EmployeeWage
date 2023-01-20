2602b3a83b05bcb3175d4620d2c743bf18f98c4c
devraajmadhuri@gmail.com <devraajmadhuri@gmail.com>
2023-01-20 09:08:58 +0530

    Added:to check weather employee is present or absent

---
+++ Employee.js
@@ -0,0 +1,7 @@
+const IS_Absent=0;
+let empCheck=Math.floor(Math.random()*10)%2;
+if(empCheck==IS_Absent){
+    console.log("Employee is absent");
+}else{
+    console.log("Employee is present");
+}
